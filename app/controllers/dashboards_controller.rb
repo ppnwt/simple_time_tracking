bf8fa3dae96a91d853a6c49f3e3eb822592df3dd
newzpanuwat <new_sbh@me.com>
2017-05-21 22:53:53 +0700

    fixed bugs

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,6 @@ class DashboardsController < ApplicationController
     def index
         @posts = Post.where(["description LIKE ?", "%#{params[:search]}%"])
         @posts_current = Post.where(user_id: current_user.id) 
-        @leavedays = Leaveday.all.order("created_at DESC")
+        @leavedays = Leaveday.all
     end
 end
