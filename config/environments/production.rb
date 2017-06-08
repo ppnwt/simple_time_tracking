cb452b2a6f63b7041e16789d9fec48cedd021e10
newzpanuwat <new_sbh@me.com>
2017-06-08 22:35:59 +0700

    change mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ end
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => "http://simple-time-tracker.herokuapp.com" }
+
 end
