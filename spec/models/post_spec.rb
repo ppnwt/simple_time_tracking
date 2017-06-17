1ebd65d22ed492738a312a17b2dcbe1c6f29f4cc
newzpanuwat <new_sbh@me.com>
2017-06-17 23:41:49 +0700

    fix somethings

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
 
-     it { is_expected.to validate_presence_of(:description) }
-     it { is_expected.to validate_length_of(:description) }
-      it { is_expected.to validate_presence_of(:timein) }
-       it { is_expected.to validate_presence_of(:timeout) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_length_of(:description) }
+    it { is_expected.to validate_presence_of(:timein) }
+    it { is_expected.to validate_presence_of(:timeout) }
 
 end
