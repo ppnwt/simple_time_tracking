6bc264cbbf2cbf96b6b65bb94e66c310e1df435e
newzpanuwat <new_sbh@me.com>
2017-09-15 15:10:10 +0700

    del vs code and add schdule

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,28 +1,23 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :destroy]
   before_action :authenticate_user!
-  
-  # GET /posts
-  # GET /posts.json
+
   def index
       @posts = Post.where(user_id: current_user.id)
   end
-  # GET /posts/1
-  # GET /posts/1.json
+
   def show
   end
 
-  # GET /posts/new
   def new
     @post = current_user.posts.build
   end
 
-  # GET /posts/1/edit
+
   def edit
   end
 
-  # POST /posts
-  # POST /posts.json
+
   def create
     @post = current_user.posts.build(post_params)
 
@@ -37,8 +32,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -51,8 +44,7 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
+
   def destroy
     @post.destroy
     respond_to do |format|
@@ -62,12 +54,11 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
       params.require(:post).permit(:date, :timein, :timeout, :description)
     end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,11 +7,8 @@ RSpec.describe Post, type: :model do
     #Validation of description
     it { is_expected.to validate_presence_of(:description) }
     it { is_expected.to validate_length_of(:description) }
-
     #Validation of time in
     it { is_expected.to validate_presence_of(:timein) }
-
-
     #Validation of timeout
     it { is_expected.to validate_presence_of(:timeout) }
 
